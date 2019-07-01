95403566d82287b6ddb209d1c5eb99de9cd7c032
lzxm160 <lzxm160@sina.com>
2019-07-01 12:57:20 +0800

    x

--- crypto/key_test.go
+++ crypto/key_test.go
@@ -8,6 +8,7 @@ package crypto
 
 import (
 	"encoding/hex"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -79,10 +80,11 @@ func TestCompressDecompress(t *testing.T) {
 	require.NoError(err)
 	pub, err := DecompressPubkey(compressd)
 	require.NoError(err)
-	//fmt.Println(pub.EcdsaPublicKey().X.Text(10))
-	//fmt.Println(pub.EcdsaPublicKey().Y.String())
-	//require.Equal("40a609475afa1f9a784cad0db5d5ba7dbaab2147a5d7b9bbde4d1334a0e40a5e", pub.EcdsaPublicKey().X.Text(10))
-	//require.Equal("188ac3f1c6bbbc336fdc33cb5e605ff7c3ee2d36249933b0322220a616a11fb3", pub.EcdsaPublicKey().Y.String())
+
+	fmt.Println(hex.EncodeToString(pub.EcdsaPublicKey().X.Bytes()))
+	fmt.Println(hex.EncodeToString(pub.EcdsaPublicKey().Y.Bytes()))
+	require.Equal("40a609475afa1f9a784cad0db5d5ba7dbaab2147a5d7b9bbde4d1334a0e40a5e", pub.EcdsaPublicKey().X.Text(10))
+	require.Equal("188ac3f1c6bbbc336fdc33cb5e605ff7c3ee2d36249933b0322220a616a11fb3", pub.EcdsaPublicKey().Y.String())
 	com := CompressPubkey(pub)
 	require.Equal(compressedString, hex.EncodeToString(com))
 }
