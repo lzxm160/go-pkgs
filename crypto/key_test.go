912ab9c98a8b7ae329353913d78f8a76e35d8b3f
lzxm160 <lzxm160@sina.com>
2020-01-14 09:18:44 +0800

    x

--- crypto/key_test.go
+++ crypto/key_test.go
@@ -16,6 +16,9 @@ import (
 const (
 	publicKey  = "04403d3c0dbd3270ddfc248c3df1f9aafd60f1d8e7456961c9ef26292262cc68f0ea9690263bef9e197a38f06026814fc70912c2b98d2e90a68f8ddc5328180a01"
 	privateKey = "82a1556b2dbd0e3615e367edf5d3b90ce04346ec4d12ed71f67c70920ef9ac90"
+
+	sm2publicKey  = "0490aca9f9b0184b1fd8960a358a8496444bf9ae1858619b6d5f2e23436d180464e355d65758e1980ad94645b3654845f4add0fc5a5fd48e11795a36192f76a5d9"
+	sm2privateKey = "b051d4a19248b1838e7101f4a7899513b305318972c4b818f09b87b9da926104"
 )
 
 func TestKeypair(t *testing.T) {
@@ -59,6 +62,50 @@ func TestKeypair(t *testing.T) {
 	require.Error(err)
 
 	_, err = StringToPubKeyBytes(publicKey, false)
+	require.NoError(err)
+}
+
+func TestSm2Keypair(t *testing.T) {
+	require := require.New(t)
+
+	_, err := HexStringToPublicKey("", true)
+	require.True(strings.Contains(err.Error(), "invalid secp256k1 public key"))
+	_, err = HexStringToPrivateKey("", true)
+	require.True(strings.Contains(err.Error(), "invalid length, need 256 bits"))
+
+	pubKey, err := HexStringToPublicKey(sm2publicKey, true)
+	require.NoError(err)
+
+	pubKey2, err := HexStringToPublicKey(sm2publicKey[2:], true)
+	require.NoError(err)
+	require.Equal(pubKey, pubKey2)
+
+	priKey, err := HexStringToPrivateKey(sm2privateKey, true)
+	require.NoError(err)
+
+	require.Equal(sm2publicKey, pubKey.HexString())
+	require.Equal(sm2privateKey, priKey.HexString())
+
+	pubKeyBytes := pubKey.Bytes()
+	priKeyBytes := priKey.Bytes()
+
+	_, err = BytesToPublicKey([]byte{1, 2, 3}, true)
+	require.Error(err)
+	_, err = BytesToPrivateKey([]byte{4, 5, 6}, true)
+	require.Error(err)
+
+	pk, err := BytesToPublicKey(pubKeyBytes, true)
+	require.NoError(err)
+	sk, err := BytesToPrivateKey(priKeyBytes, true)
+	require.NoError(err)
+
+	require.Equal(sm2publicKey, pk.HexString())
+	require.Equal(sm2privateKey, sk.HexString())
+
+	_, err = StringToPubKeyBytes("", true)
+	require.Error(err)
+
+	_, err = StringToPubKeyBytes(sm2publicKey, true)
 	require.NoError(err)
 }
 
